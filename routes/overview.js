68855dc0014df7881ee1af6220a45b0d386b0905
Michael Sudhaus <mike.sudhaus@gmail.com>
2020-08-27 16:30:29 +0200

    Added Form to input Artists

--- routes/overview.js
+++ routes/overview.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var artists = []
+
 /* GET overview page. */
 router.get('/', function(req, res, next) {
 
     res.render('overview')
 
 });
+
+/* Input of Artist */
+router.post('/artistInput', function(req,res){
+
+    artists.push(req.body['artist'])
+
+    console.log(artists)
+
+    res.redirect('/overview')
+})
 
 module.exports = router;
