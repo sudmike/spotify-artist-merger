d00997d55c41e9a9970ec41092362fd374a73357
Michael Sudhaus <mike.sudhaus@gmail.com>
2020-08-31 17:31:08 +0200

    Added Algorithm to create song lists for artists

--- public/javascripts/tools.js
+++ public/javascripts/tools.js
@@ -22,8 +22,7 @@ module.exports = {
                         response = Promise.resolve(data.body.artists.items[0].name)
                     }
                 }
-            }
-            )
+            })
             .catch(function(err) {
                 /* Error Handling */
                 response = Promise.reject(new Error("Artist fetch Error"))
@@ -35,14 +34,20 @@ module.exports = {
         console.log('Title: ', generateTitle(artists))
         console.log('Description: ', generateDescription(artists))
 
+        await generateSongList(spotifyApi, artists)
+
+        /*
         spotifyApi.createPlaylist(await getUsername(spotifyApi), generateTitle(artists), {'public' : false, 'description' : generateDescription(artists)})
             .then(function(data){
                 console.log('Playlist ID: ', data.body.id)
+                spotifyApi.addTracksToPlaylist(data.body.id, generateSongList(spotifyApi, artists))
             })
             .catch(function(err){
-                /* Error Handling */
+                /* Error Handling /
                 console.log('Playlist could not be created: ', err)
             })
+
+         */
     }
 }
 
@@ -89,4 +94,118 @@ var getUsername = async function(spotifyApi){
             // Error Handling //
             console.log('Could not get User', err)
         })
+}
+
+var generateSongList = async function(spotifyApi, artists){
+    //var songList =[]
+    //create song List from each artists This Is playlist
+    //for(var i = 0; i < artists.length; i++){
+        //songList += extractTracksOfArtist(spotifyApi, artists[i], 25)
+    //}
+
+    console.log(await extractTracksOfArtist(spotifyApi, artists[0], 25))
+
+}
+
+var extractTracksOfArtist = async function(spotifyApi, artist, nrOfSongs){
+    var songURIs = []
+
+    //get the 'This is <artist>' playlist ID to then search for playlist
+    return getThisIsPlaylistId(spotifyApi, artist)
+        .then(function(playlistId) {
+
+            //get Playlist with all tracks
+            return spotifyApi.getPlaylist(playlistId)
+                .then(function(data){
+                    if(data.statusCode !== 200){
+                        /* Error Handling */
+                        console.log('Code from Spotify that is not 200')
+                        return Promise.reject(new Error('Code from Spotify that is not 200'))
+                    }
+                    else{
+                        //Transfer all Track URIs != null from Playlist to array
+                        for(var i = 0; i < data.body.tracks.items.length; i++){
+                            if(data.body.tracks.items[i].track !== null) songURIs.push(data.body.tracks.items[i].track.uri + i)
+                        }
+
+                        //return filtered array filled with Track URIs
+                        return Promise.resolve(trimSongSelection(songURIs, nrOfSongs))
+                    }
+                })
+                .catch(function(err){
+                    /* Error Handling */
+                    console.log('An artists page could not be retrieved', err)
+                    return Promise.reject(new Error('An artists page could not be retrieved'))
+                })
+        })
+        .catch(function(err){
+            /* Error Handling */
+            console.log(err)
+            return Promise.reject(new Error(err))
+        })
+}
+
+var getThisIsPlaylistId = async function(spotifyApi, artist){
+     return spotifyApi.searchPlaylists('This Is ' + artist, {limit:1,offset:0})
+        .then(function(data){
+            if(data.statusCode !== 200){
+                /* Error Handling */
+                return Promise.reject(new Error('Error Code from Spotify that is not 200'))
+            }
+            else if(data.body.playlists.items[0].owner.id !== 'spotify'){
+                /* Local Error Handling */ //better check in the beginning
+                return Promise.reject(new Error('There is no This Is page for an artist'))
+            }
+            else{ //passed all checks
+                return Promise.resolve(data.body.playlists.items[0].id)
+            }
+        })
+        .catch(function(err){
+            console.log(err)
+            return Promise.reject(new Error('An artists page could not be retrieved'))
+        })
+
+}
+
+var trimSongSelection = function(songList, nrOfSongs = songList.length/3){
+
+    //No need to go through if all will be returned anyway
+    if(songList.length <= nrOfSongs) return songList
+    else{
+        //Split Songs into categories based on popularity
+        var hotSongs = songList.slice(0, songList.length*(1/6))
+        var mediumSongs = songList.slice(hotSongs.length, hotSongs.length+songList.length*(1/4))
+        var coldSongs = songList.slice(mediumSongs.length)
+
+        //Shuffle Song pools once
+        hotSongs = shuffleArray(hotSongs)
+        mediumSongs = shuffleArray(mediumSongs)
+        coldSongs = shuffleArray(coldSongs)
+
+        songList = []
+
+        //Randomly Select a song pool to add a song to the song list Hunger Games Style. Songs in hotter pools have a higher chance to be picked
+        for(var i = 0; i < nrOfSongs; i++){
+            var randomRealm = hotSongs.length*3 + mediumSongs.length*2 + coldSongs.length
+            var randomSelection = Math.random()*randomRealm
+
+            //console.log('H:', hotSongs.length, ', M:', mediumSongs.length, ', C:', coldSongs.length, '. Realm: ', randomRealm, ', Selection: ', randomSelection)
+
+            if(randomSelection <= hotSongs.length*3) songList.push(hotSongs.pop())
+            else if(randomSelection <= hotSongs.length*3 + mediumSongs.length*2) songList.push(mediumSongs.pop())
+            else songList.push(coldSongs.pop())
+        }
+
+        return songList
+    }
+}
+
+var shuffleArray = function(array){
+    for(let i = array.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array
 }
