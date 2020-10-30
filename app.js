35a2b7db4f6bd312d2a714569a4e82160ef50eb0
sudmike <mike.sudhaus@gmail.com>
2020-10-30 13:09:49 +0100

    missed file

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var SpotifyWebApi = require('spotify-web-api-node')
+var SpotifyWebApi = require('spotify-web-api-node');
 
-//Router definitions
 var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
-var overviewRouter = require('./routes/overview')
-var templateRouter = require('./routes/template')
-var errorRouter = require('./routes/error')
+var serverRouter = require('./routes/server');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'vash');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,37 +15,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Connect Router to URL
 app.use('/', indexRouter);
-app.use('/login', loginRouter)
-app.use('/overview', overviewRouter)
-app.use('/template', templateRouter)
-app.use('/error', errorRouter)
+app.use('/api', serverRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+module.exports = app;
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 
 var spotifyApi = new SpotifyWebApi({
-  clientId: '8c08a50634a84a1f8786e261409f71e4',
-  clientSecret: '',
-  redirectUri: 'http://localhost:3000/login/callback'
+    clientId: '8c08a50634a84a1f8786e261409f71e4',
+    clientSecret: '',
+    redirectUri: 'http://localhost:3000/api/callback'
 });
 
 global.spotify = { spotifyApi: spotifyApi };
-//Disgusting
-
-module.exports = app;
